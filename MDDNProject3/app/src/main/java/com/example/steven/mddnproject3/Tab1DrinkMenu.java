e4164d7e6e3206ffdb739dfb24c54e2ca600806e
Steven <Steven.morrison1992@gmail.com>
2017-05-24 13:05:57 +1200

    Added all buttons for UX

--- MDDNProject3/app/src/main/java/com/example/steven/mddnproject3/Tab1DrinkMenu.java
+++ MDDNProject3/app/src/main/java/com/example/steven/mddnproject3/Tab1DrinkMenu.java
@@ -10,14 +10,21 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-public class Tab1DrinkMenu extends Fragment{
+public class Tab1DrinkMenu extends Fragment implements View.OnClickListener {
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.tab1drinkmenu, container, false);
         return rootView;
+
     }
 
+    @Override
+    public void onClick(View v) {
+
+    }
 }

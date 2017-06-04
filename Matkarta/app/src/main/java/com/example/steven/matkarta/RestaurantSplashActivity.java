2ea7b9c3a836c325e023f1b6e800a2a43120510d
Char8125 <chris2pherancheta@yahoo.com>
2017-06-04 16:31:35 +1200

    Test Image Button

--- Matkarta/app/src/main/java/com/example/steven/matkarta/RestaurantSplashActivity.java
+++ Matkarta/app/src/main/java/com/example/steven/matkarta/RestaurantSplashActivity.java
@@ -1,13 +1,32 @@
 package com.example.steven.matkarta;
 
-import android.os.Bundle;
+
+
+import android.support.design.widget.TabLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.TextView;
+
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class RestaurantSplashActivity extends AppCompatActivity {
+
+    ImageButton splashMenuButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +44,23 @@ public class RestaurantSplashActivity extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+
+        splashMenuButton = (ImageButton) findViewById(R.id.splashMenuButton);
+
+        splashMenuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(RestaurantSplashActivity.this, "It Works", Toast.LENGTH_LONG).show();
+            }
+
+
+
+
+        });
+
+
     }
 
 }

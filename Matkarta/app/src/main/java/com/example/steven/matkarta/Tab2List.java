fe3aca9c95ebc52beeea9c5280b27d98e60faa39
ashtondavvo <ashton.d173@live.com>
2017-06-04 15:30:11 +1200

    Changed to hashmap and transition to splash screen added

--- Matkarta/app/src/main/java/com/example/steven/matkarta/Tab2List.java
+++ Matkarta/app/src/main/java/com/example/steven/matkarta/Tab2List.java
@@ -7,6 +7,7 @@ package com.example.steven.matkarta;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -14,6 +15,7 @@ import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
@@ -27,13 +29,14 @@ import com.google.firebase.database.GenericTypeIndicator;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Tab2List extends Fragment{
 
     DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
-    DatabaseReference listReference = mRootRef.child("Restraunts");
+    DatabaseReference listReference = mRootRef.child("Restaurants");
     private ListView restrauntNames;
-    ArrayList<String> response = new ArrayList<String>();
+    HashMap<String, String> response = new HashMap<String, String>();
     ArrayAdapter<String> adapter;
     Context context;
 
@@ -43,23 +46,36 @@ public class Tab2List extends Fragment{
         context = this.getContext();
         View rootView = inflater.inflate(R.layout.tab2list, container, false);
         restrauntNames = (ListView) rootView.findViewById(R.id.restrauntListView);
-
+        restrauntNames.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                System.out.println("--------------------------------------------------------------");
+                System.out.println("Parent: " + parent);
+                System.out.println("View: " + view);
+                System.out.println("Position: " + position);
+                System.out.println("ID: " + id);
+                System.out.println("--------------------------------------------------------------");
+                Intent intent = new Intent(getContext(), RestaurantSplashActivity.class);
+                intent.putExtra("Restaurant_ID", id);
+                startActivity(intent);
+            }
+        });
         listReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                GenericTypeIndicator<ArrayList<String>> t = new GenericTypeIndicator<ArrayList<String>>() {};
+                GenericTypeIndicator<HashMap<String,String>> t = new GenericTypeIndicator<HashMap<String, String>>() {};
                 response = dataSnapshot.getValue(t);
-                System.out.println("--------------------------------------------------------------");
-                System.out.println(response);
-                System.out.println(restrauntNames);
-                System.out.println(context);
-                System.out.println("--------------------------------------------------------------");
-                adapter = new ArrayAdapter<String>(context, R.layout.list_button, response);
+//                System.out.println("--------------------------------------------------------------");
+//                System.out.println(response);
+//                System.out.println(restrauntNames);
+//                System.out.println(context);
+//                System.out.println("--------------------------------------------------------------");
+                ArrayList<String> names = new ArrayList<String>(response.keySet());
+                adapter = new ArrayAdapter<String>(context, R.layout.list_button, names);
+
                 restrauntNames.setAdapter(adapter);
                 adapter.notifyDataSetChanged();
-                for(String s: response){
 
-                }
             }
 
             @Override

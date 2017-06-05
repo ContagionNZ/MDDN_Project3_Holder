b888f79601e0846e42568a63d7c3fda598a033f2
Char8125 <chris2pherancheta@yahoo.com>
2017-06-05 17:18:30 +1200

    JavaFix

---
+++ src/main/java/com/example/steven/matkarta/RestaurantSplashActivity.java
@@ -0,0 +1,64 @@
+package com.example.steven.matkarta;
+
+
+
+import android.support.design.widget.TabLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.TextView;
+
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+public class RestaurantSplashActivity extends AppCompatActivity {
+
+    ImageButton splashMenuButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant_splash);
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+//
+//        splashMenuButton = (ImageButton) findViewById(R.id.splashMenuButton);
+//        System.out.println(splashMenuButton);
+//        splashMenuButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(RestaurantSplashActivity.this, "It Works", Toast.LENGTH_LONG).show();
+//            }
+//
+//
+//
+//
+//        });
+
+
+    }
+
+}
---
+++ src/main/java/com/example/steven/matkarta/Tab2List.java
@@ -0,0 +1,119 @@
+package com.example.steven.matkarta;
+
+/**
+ * Created by Steven on 23/05/2017.
+ */
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.database.DataSetObserver;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Tab2List extends Fragment{
+
+    DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference listReference = mRootRef.child("Restaurants");
+    private ListView restrauntNames;
+    HashMap<String, String> response = new HashMap<String, String>();
+    ArrayAdapter<String> adapter;
+    ArrayList<String> names = new ArrayList<String>();
+    Context context;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        context = this.getContext();
+        View rootView = inflater.inflate(R.layout.tab2list, container, false);
+        restrauntNames = (ListView) rootView.findViewById(R.id.restrauntListView);
+        restrauntNames.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                System.out.println("--------------------------------------------------------------");
+                System.out.println("Parent: " + parent);
+                System.out.println("View: " + view);
+                System.out.println("Position: " + position);
+                System.out.println("ID: " + id);
+                System.out.println("--------------------------------------------------------------");
+                Intent intent = new Intent(getContext(), RestaurantSplashActivity.class);
+                intent.putExtra("Restaurant_ID", names.get(position));
+                startActivity(intent);
+            }
+        });
+        listReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                GenericTypeIndicator<HashMap<String,String>> t = new GenericTypeIndicator<HashMap<String, String>>() {};
+                response = dataSnapshot.getValue(t);
+//                System.out.println("--------------------------------------------------------------");
+//                System.out.println(response);
+//                System.out.println(restrauntNames);
+//                System.out.println(context);
+//                System.out.println("--------------------------------------------------------------");
+                names = new ArrayList<String>(response.keySet());
+                adapter = new ArrayAdapter<String>(context, R.layout.list_button, names);
+
+                restrauntNames.setAdapter(adapter);
+                adapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+        return rootView;
+    }
+
+//    @Override
+//    public void onStart(){
+//        super.onStart();
+//        Activity current = getActivity();
+//        adapter = new ArrayAdapter<String>(current, android.R.layout.simple_list_item_1, response);
+//
+//        restrauntNames.setAdapter(adapter);
+//        adapter.notifyDataSetChanged();
+//        //reponse =
+//        listReference.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                GenericTypeIndicator<ArrayList<String>> t = new GenericTypeIndicator<ArrayList<String>>() {};
+//                response = dataSnapshot.getValue(t);
+//
+//                restrauntNames.setAdapter(adapter);
+//                adapter.notifyDataSetChanged();
+//                for(String s: response){
+//
+//                }
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError databaseError) {
+//
+//            }
+//        });
+//    }
+
+}

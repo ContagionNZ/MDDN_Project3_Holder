02c11dc92952133889029ea65719e66278ad5bff
ashtondavvo <ashton.d173@live.com>
2017-06-07 13:44:49 +1200

    added formating for drinks tab

--- Matkarta/app/src/main/java/com/example/steven/matkarta/Tab1Drinks.java
+++ Matkarta/app/src/main/java/com/example/steven/matkarta/Tab1Drinks.java
@@ -10,10 +10,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.Fragment;
+import android.text.InputType;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.ScrollView;
@@ -48,6 +54,7 @@ public class Tab1Drinks extends Fragment{
         context = this.getContext();
         scrollView = (ScrollView) rootView.findViewById(R.id.drinkScroll);
         drinkLayout = (LinearLayout) rootView.findViewById(R.id.drinkLayout);
+        drinkLayout.setOrientation(LinearLayout.VERTICAL);
         Intent intent = getActivity().getIntent();
         String restaurantName = intent.getStringExtra("Restaurant_ID");
         System.out.println(restaurantName + "_Menu");
@@ -58,7 +65,7 @@ public class Tab1Drinks extends Fragment{
                 GenericTypeIndicator<HashMap<String,HashMap<String,String>>> t = new GenericTypeIndicator<HashMap<String, HashMap<String, String>>>() {};
                 response = dataSnapshot.getValue(t);
                 drinks = response.get("Drinks");
-
+                drinkLayout.removeAllViews();
                 for(String s : drinks.keySet()){
                     // create linearLayout which will represent a row
                     LinearLayout linearLayout = new LinearLayout(context);
@@ -68,13 +75,66 @@ public class Tab1Drinks extends Fragment{
                     TextView drinkName = new TextView(context);
                     drinkName.setText(s);
                     linearLayout.addView(drinkName);
+                    DisplayMetrics dm = getResources().getDisplayMetrics();
+                    float dpInPx = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 150, dm);
+                    drinkName.getLayoutParams().width = (int)dpInPx;
+                    dpInPx = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 50, dm);
+                    drinkName.getLayoutParams().height = (int)dpInPx;
+                    drinkName.setTextSize(20);
+                   // drinkName.setGravity(Gravity.CENTER_VERTICAL);
+
                     // create the drink price
                     TextView drinkPrice = new TextView(context);
                     drinkPrice.setText(drinks.get(s));
+                    drinkPrice.setTextSize(20);
                     linearLayout.addView(drinkPrice);
+                    dpInPx = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 90, dm);
+                    drinkPrice.getLayoutParams().width = (int)dpInPx;
+                 //   drinkPrice.setGravity(Gravity.CENTER_VERTICAL);
+
+                    // create the minus button
+                    ImageButton minusButton = new ImageButton(context);
+                    minusButton.setImageResource(R.drawable.minius_mexico);
+                    minusButton.setBackgroundResource(0);
+                    linearLayout.addView(minusButton);
+                    dpInPx = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 30, dm);
+                    minusButton.getLayoutParams().width = (int) dpInPx;
+                    minusButton.getLayoutParams().height = (int) dpInPx;
+
+                    minusButton.requestLayout();
+
+                    // create the counter
+                    final TextView counter = new TextView(context);
+                    counter.setInputType(InputType.TYPE_CLASS_NUMBER);
+                    counter.setText("0");
+                    counter.setTextSize(20);
+                    linearLayout.addView(counter);
+                    dpInPx = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 50, dm);
+                    counter.getLayoutParams().width = (int) dpInPx;
+                    counter.setGravity(Gravity.CENTER_HORIZONTAL);
+
+                    // create the plus button
+                    ImageButton plusButton = new ImageButton(context);
+                    plusButton.setImageResource(R.drawable.add_mexico);
+                    plusButton.setBackgroundResource(0);
+                    linearLayout.addView(plusButton);
+                    dpInPx = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 30, dm);
+                    plusButton.getLayoutParams().width = (int) dpInPx;
+                    plusButton.getLayoutParams().height = (int) dpInPx;
+                    plusButton.requestLayout();
 
 
+                    // Set the buttonClickListeners
+                    plusButton.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            int current = Integer.parseInt(counter.getText().toString());
+                            System.out.println(current);
+                        }
+                    });
 
+
+                    drinkLayout.addView(linearLayout);
                 }
 
 

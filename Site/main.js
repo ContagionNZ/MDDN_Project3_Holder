cadfd9b2d65b0d8102988d4c8082f3ca6a656385
Steven Morrison <morrisstev1@h-05-07-lab.mddn.vuw.ac.nz>
2017-06-07 14:03:20 +1200

    Added updates to the site with working firebase database overwirghting

--- Site/main.js
+++ Site/main.js
@@ -1,8 +1,38 @@
 var fireText = document.getElementById("fireText");
+var fireTextTwo = document.getElementById("fireTextTwo");
+var fireTextThree = document.getElementById("fireTextThree");
 
-var firebaseTextRef = firebase.database().ref().child("Test");
+var firebaseTextRef = firebase.database().ref().child("Test one");
 
 firebaseTextRef.on('value', function(datasnapshot){
 	fireText.innerText = datasnapshot.val();
-	console.log(datasnapshot);
 });
+
+var firebaseTextRefTwo = firebase.database().ref().child("Test Two");
+
+	firebaseTextRefTwo.on('value' , function(datasnapshot){
+		fireTextTwo.innerText = datasnapshot.val();
+
+	});
+
+var firebaseTextRefThree = firebase.database().ref().child("Test Three");
+
+	firebaseTextRefThree.on('value' , function(datasnapshot){
+		fireTextThree.innerText = datasnapshot.val();
+
+	});
+
+	function doFunction(){
+
+		firebase.database().ref("Test one").set('Order');
+	}
+
+	function doFunctionTwo(){
+
+		firebase.database().ref("Test Two").set('Order');
+	}
+
+	function doFunctionThree(){
+
+		firebase.database().ref("Test Three").set('Order');
+	}
